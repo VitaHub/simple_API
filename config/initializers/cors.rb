fa884a9836782178f4987b1a7c147ef7b33dcc6a
VitaHub <hubenko_vitalii@mail.ua>
2016-05-21 22:22:33 +0300

    CORS modified

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
      origins 'simple-rails-api.herokuapp.com'
 
      resource '*',
-       headers: :any,
+       headers: 'Access-Control-Allow-Origin',
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
    end
  end
